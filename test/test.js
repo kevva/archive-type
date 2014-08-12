426f8efb4f17cc22ff248308edfb2db56caf3e00
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-13 00:20:05 +0200

    Use ava for testing

--- test/test.js
+++ test/test.js
@@ -1,19 +1,17 @@
-/*global describe, it */
 'use strict';
 
-var assert = require('assert');
 var archiveType = require('../');
 var fs = require('fs');
 var path = require('path');
+var test = require('ava');
 
-describe('extname()', function () {
-    function check(file) {
+test('detect archive type from Buffer', function (t) {
+    t.plan(2);
+
+    function c(file) {
         return archiveType(fs.readFileSync(file));
     }
 
-    it('should detect archive type from Buffer', function (cb) {
-        assert.strictEqual(check(path.join(__dirname, 'fixtures/test.tar')), 'tar');
-        assert.strictEqual(check(path.join(__dirname, 'fixtures/test.zip')), 'zip');
-        cb();
-    });
+    t.assert(c(path.join(__dirname, 'fixtures/test.tar')) === 'tar');
+    t.assert(c(path.join(__dirname, 'fixtures/test.zip')) === 'zip');
 });
