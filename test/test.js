1358a64b501be2e9ceedffd7382a5a365fae9ec7
XiNGRZ <hi@xingrz.me>
2021-11-02 00:06:25 +0800

    Add Zstandard (zstd)

--- test/test.js
+++ test/test.js
@@ -7,9 +7,12 @@ import m from '..';
 test(async t => {
 	const tar = await pify(fs.readFile)(path.join(__dirname, 'fixtures/test.tar'));
 	const zip = await pify(fs.readFile)(path.join(__dirname, 'fixtures/test.zip'));
+	const zst = await pify(fs.readFile)(path.join(__dirname, 'fixtures/test.zst'));
 
 	t.is(m(tar).ext, 'tar');
 	t.is(m(tar).mime, 'application/x-tar');
 	t.is(m(zip).ext, 'zip');
 	t.is(m(zip).mime, 'application/zip');
+	t.is(m(zst).ext, 'zst');
+	t.is(m(zst).mime, 'application/zstd');
 });
